572c25a3298c923d2c129d2ff50ec876e79bb449
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-04-30 14:28:42 -0600

    Add code for angular 19 + tailwind

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# GifsApp.
-This is a repository for show api.giphy.com using angular 19 + tailwindcss
+# GifsApp
+
+Este un proyecto de Angular.
+
+## Dev
+
+1. Clonar el repositorio
+2. Instalar las dependencias con `npm install`
+3. Iniciar el servidor con `ng serve`
+4. Navegar a `http://localhost:4200/`
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'IdvFByhuq0EcabV8oUh8kjLd9Eguiuz4',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
